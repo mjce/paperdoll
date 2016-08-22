f664e5c7c605cb8973a51ace8d3ef228497c3e33
mjce <ce.alterego@gmail.com>
2016-08-22 09:22:57 -0400

    pruning directory

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require('./app/index')
+const path = require('path')  
+const express = require('express')  
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.engine('hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views'))
+app.get('/', (request, response) => {  
+  response.render('paperdoll', {})
+})
+
+app.listen(8000)
