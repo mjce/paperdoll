175386da752e50658467d02c3d028e0c28910d9e
mjce <ce.alterego@gmail.com>
2016-08-22 12:34:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PouchDB = require('pouchdb')
-var cors = require('cors')
 
 app.engine('hbs', exphbs({  
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'client')));
 app.use('/db', require('express-pouchdb')(PouchDB));
+var inventoryDB = new PouchDB('inventory');
 
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))
