0e34284f97d78154c17974eae5848e27b8f2c509
mjce <ce.alterego@gmail.com>
2016-08-22 14:32:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PouchDB = require('pouchdb')
+const cors = require('cors')
 
 app.engine('hbs', exphbs({  
   defaultLayout: 'main',
@@ -10,13 +11,7 @@ app.engine('hbs', exphbs({
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
 app.use(express.static(path.join(__dirname, 'client')));
-app.use('/db', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://descension.me");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   res.header("Access-Control-Allow-Credentials", "true");
-  require('express-pouchdb')(PouchDB)
-  next();
-  });
+app.use('/db', cors(), require('express-pouchdb')(PouchDB));
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))
 app.get('/', (request, response) => {
