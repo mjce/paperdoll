dab65deec588fead0b736c32dcc2471fe77a26b0
mjce <ce.alterego@gmail.com>
2016-08-22 11:16:24 -0400

    Update paperdoll.js

--- client/paperdoll.js
+++ client/paperdoll.js
@@ -1,4 +1,4 @@
-var inventoryDB = new PouchDB('http://localhost:8000/db/inventory');
+var inventoryDB = new PouchDB('http://localhost:8000/db');
 var remoteCouch = false;
 addInventoryItem("test", 1, true, "weapon", "dummy stats", "dummy bonus stats");
 
